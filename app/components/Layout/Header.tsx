67b7404420942d6832c24e9502accf54f646448f
carmelc <carmelc@wix.com>
2023-01-29 14:00:06 +0200

    feat(Headless): header small tweaks

--- app/components/Layout/Header.tsx
+++ app/components/Layout/Header.tsx
@@ -2,12 +2,16 @@ import { NavBar } from '@app/components/Layout/NavBar/NavBar';
 
 const Header = () => (
   <>
-    <header className="fixed h-header bg-white z-40 w-full">
+    <header className="absolute md:fixed h-header bg-white z-40 w-full">
       <div className="relative flex justify-center max-w-full-content mx-auto gap-8 h-header items-center">
-        <div className="flex flex-col justify-between items-center min-w-[300px] ml-3">
+        <a
+          href="/"
+          target="_self"
+          className="flex flex-col justify-between items-center min-w-[300px] ml-3"
+        >
           <div className="font-lulo mb-3">ALLAN JOHNSON</div>
           <div className="text-xs tracking-widest">Personal Life Coach</div>
-        </div>
+        </a>
         <div className="flex-grow pb-5 pr-5">
           <NavBar />
         </div>
